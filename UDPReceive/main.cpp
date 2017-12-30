0f1dfa94bae8028b83fa74c8fc3a26702a2a1a30
苏 <enmoton@qq.com>
2017-12-30 14:50:59 +0800

    UDP

---
+++ UDPReceive/main.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <stdio.h>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+
+int main()
+{
+	// init
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup error !");
+		return -1;
+	}
+
+	// socket
+	SOCKET receiveSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (receiveSocket == INVALID_SOCKET)
+	{
+		printf("socket error !");
+		return -1;
+	}
+
+	// bind
+	sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(9999);
+	address.sin_addr.S_un.S_addr = INADDR_ANY;
+	if (bind(receiveSocket, (sockaddr *)&address, sizeof(address)) == SOCKET_ERROR)
+	{
+		printf("bind error !");
+		return -1;
+	}
+
+	// receive
+	char buffer[256] = { 0 };
+	sockaddr_in remoteAddr;
+	int nAddrLen = sizeof(remoteAddr);
+	int ret = recvfrom(receiveSocket, buffer, sizeof(buffer) - 1, 0, (sockaddr *)&remoteAddr, &nAddrLen);
+	if (ret > 0)
+	{
+		printf("receive length: %d \n", ret);
+		buffer[ret] = 0;
+		printf("%s", buffer);
+	}
+
+	// close
+	closesocket(receiveSocket);
+	WSACleanup();
+}
---
+++ UDPSend/main.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <stdio.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+
+int main()
+{
+	// init
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup error !");
+		return -1;
+	}
+
+	// socket
+	SOCKET sendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sendSocket == INVALID_SOCKET)
+	{
+		printf("socket error !");
+		return -1;
+	}
+
+	// send
+	sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(9999);
+	inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr);
+
+	char send[256] = "hello";
+	sendto(sendSocket, send, strlen(send), 0, (sockaddr *)&address, sizeof(address));
+
+
+	// close
+	closesocket(sendSocket);
+	WSACleanup();
+}
