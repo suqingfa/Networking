6a2e04c84ff69b34eaccfb39930ca2e302d5ceea
苏 <enmoton@qq.com>
2017-12-30 16:01:59 +0800

    TCP

---
+++ TCPClient/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <WS2tcpip.h>
+#include <winsock2.h>
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+	// init
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup error !");
+		return -1;
+	}
+
+	// socket
+	SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (client == INVALID_SOCKET)
+	{
+		printf("socket error !");
+		return -1;
+	}
+
+	// connect
+	sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(8888);
+	inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr);
+	if (connect(client, (sockaddr *)&address, sizeof(address)) == SOCKET_ERROR)
+	{
+		printf("connect error !");
+		return -1;
+	}
+
+	// send receive
+	char sendBuffer[256] = "hello";
+	send(client, sendBuffer, strlen(sendBuffer), 0);
+
+
+	closesocket(client);
+	WSACleanup();
+}
---
+++ TCPServer/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <winsock2.h>
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+	// init
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup error !");
+		return -1;
+	}
+
+	// socket
+	SOCKET server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (server == INVALID_SOCKET)
+	{
+		printf("socket error !");
+		return -1;
+	}
+
+	// bind
+	sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(8888);
+	sin.sin_addr.s_addr = INADDR_ANY;
+	if (bind(server, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)
+	{
+		printf("bind error !");
+		return -1;
+	}
+
+	// listen
+	if (listen(server, 8) == SOCKET_ERROR)
+	{
+		printf("listen error !");
+		return -1;
+	}
+
+	// accept
+	sockaddr_in remoteAddr;
+	int nAddrlen = sizeof(remoteAddr);
+	SOCKET client = accept(server, (sockaddr *)&remoteAddr, &nAddrlen);
+	if (client == INVALID_SOCKET)
+	{
+		printf("accept error !");
+		return -1;
+	}
+
+	// send receive
+	char buffer[256];
+	int ret = recv(client, buffer, sizeof(buffer), 0);
+	if (ret > 0)
+	{
+		buffer[ret] = 0;
+		printf("%s \n", buffer);
+	}
+
+	closesocket(client);
+
+	closesocket(server);
+	WSACleanup();
+}
